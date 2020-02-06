72b8261afec4009148bc41384cb62dca3868291b
BrightnessTM <khanolympics@gmail.com>
2020-02-06 09:29:30 -0500

    Updates role

---
+++ commands/updates.js
@@ -0,0 +1,21 @@
+const { main_guild } = require("../config.json");
+module.exports = {
+	controls: {
+		permission: 10,
+		usage: "updates",
+		aliases: ["joinupdates"],
+		description: "Adds/Removes the Updates role from you",
+		enabled: true,
+		permissions: ["VIEW_CHANNEL", "SEND_MESSAGES"]
+	},
+	do: (message, client, args, Discord) => {
+		if (message.guild.id !== main_guild) return;
+		if (!message.member.roles.has("670752517373820941")) {
+			message.member.addRole("670752517373820941");
+			message.channel.send("You have been given the Updates role.");
+		} else if (message.member.roles.has("670752517373820941")) {
+			message.member.removeRole("670752517373820941");
+			message.channel.send("You have been removed from the Updates role.");
+		}
+	}
+};
